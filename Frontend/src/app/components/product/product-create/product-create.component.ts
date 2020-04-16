12f64787a143447586bbf66b6d8535d96f9179c7
Bruno Ribeiro <brunomestres@gmail.com>
2020-04-16 16:47:42 -0300

    snackBar

--- Frontend/src/app/components/product/product-create/product-create.component.ts
+++ Frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-  atributoLegal = 'qualquer';
   
-  constructor() { }
+  
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
-  fazerAlgo(): void{
-    console.log("Fazendo algo");
+  createProduct(): void{
+    this.productService.showMessage('Produto criado');
+  }
+  
+  cancel(): void{
+    this.router.navigate(['/products']);
   }
 
 }
